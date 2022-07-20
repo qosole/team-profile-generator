16b1278d2b51f21af509c898d4b9cda50f74322c
qosole <williamj99wei@gmail.com>
2022-07-20 18:53:50 -0500

    Add officeNumber attribute to manager class

--- test/Manager.test.js
+++ test/Manager.test.js
@@ -4,12 +4,17 @@ const Manager = require('../lib/Manager');
 const name = 'Joe';
 const id = '2';
 const email = 'joe@email.com';
-const manager = new Manager(name, id, email);
+const officeNumber = '30';
+const manager = new Manager(name, id, email, officeNumber);
 
 describe('Manager', () => {
-    it('should inherit the attribues of Employee class', () => {
+    it('should inherit the attributes of Employee class', () => {
         expect(manager.name).toEqual(name);
         expect(manager.id).toEqual(id);
         expect(manager.email).toEqual(email);
+    })
+
+    it('should have an officeNumber attribute', () => {
+        expect(manager.officeNumber).toEqual(officeNumber);
     })
 })
