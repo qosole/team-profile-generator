c604ac8f5ea6f042f3d9a40ff83cd5dc0464d620
qosole <williamj99wei@gmail.com>
2022-07-20 18:33:27 -0500

    Add getName() method to employee class

--- test/Employee.test.js
+++ test/Employee.test.js
@@ -1,14 +1,26 @@
 const Employee = require('../lib/Employee');
 
+
+const name = 'Bob';
+const id = '12';
+const email = 'fake@email.com';
+
 describe('Employee', () => {
     it('should be created with a name, id, and email', () => {
-        const name = 'Bob';
-        const id = '12';
-        const email = 'fake@email.com';
-
         const employee = new Employee(name, id, email);
+
         expect(employee.name).toEqual(name);
         expect(employee.id).toEqual(id);
         expect(employee.email).toEqual(email);
     })
+
+    describe('getName', () => {
+        it('should return the name of the employee', () => {
+            const employee = new Employee(name, id, email);
+
+            expect(employee.getName()).toEqual(employee.name);
+        })
+    })
+
+  
 })
