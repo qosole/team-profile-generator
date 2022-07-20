f08b2b88710380af752136477d126319bee83cb5
qosole <williamj99wei@gmail.com>
2022-07-20 18:55:40 -0500

    Add tests for manager inheriting employee methods

--- test/Manager.test.js
+++ test/Manager.test.js
@@ -8,10 +8,14 @@ const officeNumber = '30';
 const manager = new Manager(name, id, email, officeNumber);
 
 describe('Manager', () => {
-    it('should inherit the attributes of Employee class', () => {
+    it('should inherit the attributes and methods of Employee class', () => {
         expect(manager.name).toEqual(name);
         expect(manager.id).toEqual(id);
         expect(manager.email).toEqual(email);
+
+        expect(manager.getName()).toEqual(manager.name);
+        expect(manager.getId()).toEqual(manager.id);
+        expect(manager.getEmail()).toEqual(manager.email);
     })
 
     it('should have an officeNumber attribute', () => {
