d20c52795a20f3935798cd058036902952c2dff2
qosole <williamj99wei@gmail.com>
2022-07-20 21:02:08 -0500

    Fix bug with user prompts not appearing after the menu

--- index.js
+++ index.js
@@ -12,12 +12,11 @@ let interns = [];
 
 const menuPrompt = () => {
     // Creating a repeating menu for the user to choose what team member they want to add next or to finish adding
-    let userMenuChoice = '';
     const nextChoices = [
         '1. Add an engineer',
         '2. Add an intern',
         '3. Finish'
-    ]
+    ];
 
     inquirer.prompt([
         {
@@ -27,73 +26,71 @@ const menuPrompt = () => {
             name: 'userMenuChoice'
         }
     ]).then(data => {
-        userMenuChoice = data.userMenuChoice;
+        // Getting user input for an engineer, creating an Engineer object, and adding it to the engineers array
+        if (data.userMenuChoice == nextChoices[0]) {
+            inquirer.prompt([
+                {
+                    message: "Enter the engineer's name: ",
+                    name: 'engineerName'
+                },
+                {
+                    message: "Enter the engineer's ID: ",
+                    name: 'engineerId'
+                },
+                {
+                    message: "Enter the engineer's email: ",
+                    name: 'engineerEmail'
+                },
+                {
+                    message: "Enter the engineer's GitHub username: ",
+                    name: 'engineerGithub'
+                }
+            ]).then(data => {
+                const newEngineer = new Engineer(data.engineerName, data.engineerId, data.engineerEmail, data.engineerGithub);
+                if (!newEngineer) {
+                    console.log('Invalid input!');
+                } else {
+                    engineers.push(newEngineer);
+                }
+                menuPrompt();
+            });
+        }
+        
+        // Getting user input for an intern, creating an Intern object, and adding it to the interns array
+        if (data.userMenuChoice == nextChoices[1]) {
+            inquirer.prompt([
+                {
+                    message: "Enter the intern's name: ",
+                    name: 'internName'
+                },
+                {
+                    message: "Enter the intern's ID: ",
+                    name: 'internId'
+                },
+                {
+                    message: "Enter the intern's email: ",
+                    name: 'internEmail'
+                },
+                {
+                    message: "Enter the intern's school: ",
+                    name: 'internSchool'
+                }
+            ]).then(data => {
+                const newIntern = new Intern(data.internName, data.internId, data.internEmail, data.internSchool);
+                if (!newIntern) {
+                    console.log('Invalid input!');
+                } else {
+                    interns.push(newIntern);
+                }
+                menuPrompt();
+            });
+        }
+
+        // Finish adding
+        if (data.userMenuChoice == nextChoices[2]) {
+            console.log('Team profile page has been generated!');
+        }
     });
-
-    // Getting user input for an engineer, creating an Engineer object, and adding it to the engineers array
-    if (userMenuChoice == nextChoices[0]) {
-        inquirer.prompt([
-            {
-                message: "Enter the engineer's name: ",
-                name: 'engineerName'
-            },
-            {
-                message: "Enter the engineer's ID: ",
-                name: 'engineerId'
-            },
-            {
-                message: "Enter the engineer's email: ",
-                name: 'engineerEmail'
-            },
-            {
-                message: "Enter the engineer's GitHub username: ",
-                name: 'engineerGithub'
-            }
-        ]).then(data => {
-            const newEngineer = new Engineer(data.engineerName, data.engineerId, data.engineerEmail, data.engineerGithub);
-            if (!newEngineer) {
-                console.log('Invalid input!');
-            } else {
-                engineers.push(newEngineer);
-            }
-            menuPrompt();
-        });
-    }
-    
-    // Getting user input for an intern, creating an Intern object, and adding it to the interns array
-    if (userMenuChoice == nextChoices[1]) {
-        inquirer.prompt([
-            {
-                message: "Enter the intern's name: ",
-                name: 'internName'
-            },
-            {
-                message: "Enter the intern's ID: ",
-                name: 'internId'
-            },
-            {
-                message: "Enter the intern's email: ",
-                name: 'internEmail'
-            },
-            {
-                message: "Enter the intern's school: ",
-                name: 'internSchool'
-            }
-        ]).then(data => {
-            const newIntern = new Intern(data.internName, data.internId, data.internEmail, data.internSchool);
-            if (!newIntern) {
-                console.log('Invalid input!');
-            } else {
-                interns.push(newIntern);
-            }
-            menuPrompt();
-        });
-    }
-
-    // Finish adding
-    if (userMenuChoice == nextChoices[2]) {
-        console.log('Team profile page has been generated!');
-    }
 }
 
 const init = () => {
