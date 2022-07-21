d7d1e4ae8073ec304915af9921412fc8d2a21c25
qosole <williamj99wei@gmail.com>
2022-07-20 20:13:31 -0500

    Fix bug with user input prompts not appearing

--- index.js
+++ index.js
@@ -6,39 +6,19 @@ const Manager = require('./lib/Manager');
 const inquirer = require('inquirer');
 
 
+let manager;
 let engineers = [];
 let interns = [];
 
-console.clear();
-console.log('Welcome to a team profile generator! Please follow the prompts to create your own team profile!')
-console.log('-----------------------------------------------------------------------------------------------');
-inquirer.prompt([
-    {
-        message: "Enter the team manager's name: ",
-        name: 'managerName'
-    },
-    {
-        message: "Enter the team manager's ID: ",
-        name: 'managerId'
-    },
-    {
-        message: "Enter the team manager's email: ",
-        name: 'managerEmail'
-    },
-    {
-        message: "Enter the team manager's office number: ",
-        name: 'managerOfficeNumber'
-    }
-]);
+const menuPrompt = () => {
+    // Creating a repeating menu for the user to choose what team member they want to add next or to finish adding
+    let userMenuChoice = '';
+    const nextChoices = [
+        '1. Add an engineer',
+        '2. Add an intern',
+        '3. Finish'
+    ]
 
-// Creating a repeating menu for the user to choose what team member they want to add next or to finish adding
-let isPrompting = true;
-const nextChoices = [
-    '1. Add an engineer',
-    '2. Add an intern',
-    '3. Finish'
-]
-while(isPrompting) {
     inquirer.prompt([
         {
             type: 'list',
@@ -46,7 +26,9 @@ while(isPrompting) {
             choices: nextChoices,
             name: 'userMenuChoice'
         }
-    ]);
+    ]).then(data => {
+        userMenuChoice = data.userMenuChoice;
+    });
 
     // Getting user input for an engineer, creating an Engineer object, and adding it to the engineers array
     if (userMenuChoice == nextChoices[0]) {
@@ -67,16 +49,17 @@ while(isPrompting) {
                 message: "Enter the engineer's GitHub username: ",
                 name: 'engineerGithub'
             }
-        ])
-
-        const newEngineer = new Engineer(engineerName, engineerId, engineerEmail, engineerGithub);
-        if (!newEngineer) {
-            console.log('Invalid input!');
-            continue;
-        }
-        engineers.push(newEngineer);
+        ]).then(data => {
+            const newEngineer = new Engineer(data.engineerName, data.engineerId, data.engineerEmail, data.engineerGithub);
+            if (!newEngineer) {
+                console.log('Invalid input!');
+            } else {
+                engineers.push(newEngineer);
+            }
+            menuPrompt();
+        });
     }
-
+    
     // Getting user input for an intern, creating an Intern object, and adding it to the interns array
     if (userMenuChoice == nextChoices[1]) {
         inquirer.prompt([
@@ -96,19 +79,47 @@ while(isPrompting) {
                 message: "Enter the intern's school: ",
                 name: 'internSchool'
             }
-        ])
-
-        const newIntern = new Intern(internName, internId, internEmail, internSchool);
-        if (!newIntern) {
-            console.log('Invalid input!');
-            continue;
-        }
-        interns.push(newIntern);
+        ]).then(data => {
+            const newIntern = new Intern(data.internName, data.internId, data.internEmail, data.internSchool);
+            if (!newIntern) {
+                console.log('Invalid input!');
+            } else {
+                interns.push(newIntern);
+            }
+            menuPrompt();
+        });
     }
 
     // Finish adding
     if (userMenuChoice == nextChoices[2]) {
         console.log('Team profile page has been generated!');
-        isPrompting = false;
     }
 }
+
+const init = () => {
+    console.log('Welcome to a team profile generator! Please follow the prompts to create your own team profile!')
+    console.log('-----------------------------------------------------------------------------------------------');
+    inquirer.prompt([
+        {
+            message: "Enter the team manager's name: ",
+            name: 'managerName'
+        },
+        {
+            message: "Enter the team manager's ID: ",
+            name: 'managerId'
+        },
+        {
+            message: "Enter the team manager's email: ",
+            name: 'managerEmail'
+        },
+        {
+            message: "Enter the team manager's office number: ",
+            name: 'managerOfficeNumber'
+        }
+    ]).then(data => {
+        manager = new Manager(data.managerName, data.managerId, data.managaerEmail, data.managerOfficeNumber);
+        menuPrompt();
+    })
+}
+
+init();
