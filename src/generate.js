0bca0071459ca40efcbe6d55f473972c347abf70
qosole <williamj99wei@gmail.com>
2022-07-21 14:53:39 -0500

    Fix bug with creating html file

--- src/generate.js
+++ src/generate.js
@@ -2,7 +2,7 @@ const fs = require('fs');
 
 
 const generate = (manager, engineers, interns) => {
-    const filePath = `../dist/team-${manager.name}.html`;
+    const filePath = `./dist/team-${manager.name}.html`;
     const pageBody = 
 `
 <!DOCTYPE html>
@@ -15,11 +15,11 @@ const generate = (manager, engineers, interns) => {
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2" crossorigin="anonymous"></script>
     <link href="./css/style.css" rel="stylesheet">
-    <title>${manager.managerName}'s Team</title>
+    <title>${manager.name}'s Team</title>
 </head>
 <body>
     <header class="card bg-success">
-        <h1>${manager.managerName}'s Team</h1>
+        <h1>${manager.name}'s Team</h1>
     </header>
 
     <div class="row row-cols-1 row-cols-md-2 g-4 w-75 mx-auto">
