fcd0fa5b5f920b1f34b5f7ff8642596b4d7b4620
qosole <williamj99wei@gmail.com>
2022-07-21 17:43:54 -0500

    Add mobile readability and provided example team profile page

---
+++ dist/scripts/helper-William.js
@@ -0,0 +1,3 @@
+
+const engineers = [{"name":"qosole","id":"33","email":"fake","github":"qosole"},{"name":"rr","id":"rr","email":"tt","github":"yy"},{"name":"yy","id":"yy","email":"uu","github":"ii"}];
+const interns = [{"name":"aaa","id":"www","email":"eee","school":""},{"name":"pp","id":"po","email":"op","school":"po"}];
